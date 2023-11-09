aaa1a5f822bf07569d358765f0a978b052ebd720
larsdeklijne <larsdeklijne@gmail.com>
2023-11-09 16:52:35 +0100

    imported shadcn table component, created own custom component from it

--- app/hypotheekrentes/page.tsx
+++ app/hypotheekrentes/page.tsx
@@ -1,6 +1,7 @@
 import CardGrid from "@/components/CardGrid"
 import Navbar from "@/components/Navbar"
 import PageIntro from "@/components/PageIntro"
+import CustomTable from "@/components/CustomTable"
 
 const page = () => {
   return (
@@ -8,7 +9,7 @@ const page = () => {
       <PageIntro title="Hypotheekrentes" button1="Huis kopen" button2="Huis huren" 
       href1="/kopen" href2="/huren"
       description="Op zoek naar de beste hypotheekrente? Wij hebben speciale deze pagina gewijd aan hypotheekrentes, waar je de nieuwste tarieven kunt vergelijken."></PageIntro>
-      <CardGrid></CardGrid>
+      <CustomTable></CustomTable>
     </div>
   )
 }
